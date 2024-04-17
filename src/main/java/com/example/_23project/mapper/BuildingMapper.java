6d9177e5b9c9e54fc07814240b489d73daa39feb
GrimaldusN <grimaldusn@gmail.com>
2024-04-17 18:14:16 +0200

    BuildingController {    createBuilding }

--- src/main/java/com/example/_23project/mapper/BuildingMapper.java
+++ src/main/java/com/example/_23project/mapper/BuildingMapper.java
@@ -3,7 +3,10 @@ package com.example._23project.mapper;
 import com.example._23project.dto.BuildingAfterCreationDto;
 import com.example._23project.dto.BuildingCreateDto;
 import com.example._23project.entity.Building;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BuildingMapper {
     Building toEntity(BuildingCreateDto buildingCreateDto);
 
