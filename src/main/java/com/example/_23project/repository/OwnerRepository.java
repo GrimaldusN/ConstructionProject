b579e9b5737f0fcba24ae9dd3cf0f4b4b3973e2f
GrimaldusN <grimaldusn@gmail.com>
2024-04-27 17:02:25 +0200

    -class Address.java -class Builder.java

--- src/main/java/com/example/_23project/repository/OwnerRepository.java
+++ src/main/java/com/example/_23project/repository/OwnerRepository.java
@@ -2,7 +2,6 @@ package com.example._23project.repository;
 
 import com.example._23project.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
--- src/main/java/com/example/_23project/sevice/OwnerService.java
+++ src/main/java/com/example/_23project/sevice/OwnerService.java
@@ -3,9 +3,14 @@ package com.example._23project.sevice;
 import com.example._23project.dto.Owner.OwnerAfterCreationDto;
 import com.example._23project.dto.Owner.OwnerCreateDto;
 import com.example._23project.entity.Owner;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OwnerService {
     Owner getOwnerById(String id);
+
+    Owner findByTellNumber(int tellNumber);
+
     void deleteOwnerById(String id);
 
     Owner updateOwnerByTellNumber(int tellNumer, int newTellNumer);
