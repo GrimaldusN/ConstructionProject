cbc46978c085126c15b4db63a856505312fc50cd
GrimaldusN <grimaldusn@gmail.com>
2024-04-28 17:27:25 +0200

    1/2 - UserControler(create)

---
+++ src/main/java/com/example/_23project/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.example._23project.mapper;
+
+import com.example._23project.dto.UserAfterCreationDto;
+import com.example._23project.dto.UserCreateDto;
+import com.example._23project.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    @Mapping(target = "login", source = "login")
+    User toEntity(UserCreateDto userCreateDto);
+    @Mapping(target = "userId",source = "id")
+    UserAfterCreationDto toDto(User userAfterCreation);
+}
